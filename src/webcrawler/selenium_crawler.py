ac62043a5696fc4ac612026339d8516c527c4c67
David Esner <esnerda@gmail.com>
2019-02-18 01:02:36 -0800

    window size

--- src/webcrawler/selenium_crawler.py
+++ src/webcrawler/selenium_crawler.py
@@ -132,7 +132,7 @@ class GenericCrawler:
             # setting for running in docker
             # TODO: remove hardcoding
             options.add_argument('--no-sandbox')
-            options.add_argument('--window-size=1920,1080')
+            options.add_argument("--window-size=1920x1080")
             options.add_argument('--headless')
             options.add_argument('--disable-gpu')
             driver = webdriver.Chrome(options=options)
